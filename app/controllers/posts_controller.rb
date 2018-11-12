7b5e957bf1d49e3a5e2c47908b165317b8c41e04
zheka <zhenkaaaa421@gmail.com>
2018-11-12 19:02:35 +0200

    update controller, model, views in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-
+  
+  before_action :authenticate_user!
+  
   def index
     
   end
@@ -7,41 +9,42 @@ class PostsController < ApplicationController
   def find_post
     @post = Post.find(params[:id])
   end
-  
 
-  def show
-    
-  end
 
   def new
-    @post = Post.new
+  @post = Post.new
   end
   
   def create
-    @post = current_user.posts.build(post_params)
-    if @post.save
-      redirect_to root_path
-    end
+  @post = current_user.posts.build(post_params)
+  if @post.save
+  redirect_to root_path
   end
-
-  def update
-    @post.update(post_params)
-    redirect_to post_path(@post.id)
   end
-
-  def edit
-    
-  end
-
-  def destroy
-    @post.destroy
-    redirect_to root_path
-  end
-
-  private
   
-  def post_params
-    params.require(:post).permit(:user_id, :name, :text)
+  def show
+  @post = Post.find_by(id:params[:id])
   end
-
+  
+  def edit
+  @post = Post.find_by(id:params[:id])
+  end
+  
+  def update
+  @post = Post.find_by(params[:id])
+  if @post.update(post_params)
+  redirect_to root_path
+  end
+  end
+  
+  def destroy
+  @post = Post.find_by(id:params[:id])
+  @post.destroy
+  redirect_to root_path
+  end
+  
+  private
+  def post_params
+  params.require(:post).permit(:user_id,:name,:text)
+  end
 end
