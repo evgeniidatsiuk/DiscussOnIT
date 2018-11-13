5497f31824abfe0d3748401e8d010630fc40a557
zheka <zhenkaaaa421@gmail.com>
2018-11-14 00:12:15 +0200

    front-end

---
+++ app/controllers/addavatars_controller.rb
@@ -0,0 +1,11 @@
+class AddavatarsController < ApplicationController
+    def self.up
+        change_table :users do |t|
+          t.attachment :avatar
+        end
+      end
+    
+      def self.down
+        drop_attached_file :users, :avatar
+      end
+end
---
+++ app/helpers/addavatars_helper.rb
@@ -0,0 +1,2 @@
+module AddavatarsHelper
+end
