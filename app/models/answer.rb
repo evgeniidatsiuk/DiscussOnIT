0eb9bb53f3edae00ac8f42345b5cc17fc511c9be
z <evgenii.datsiuk@gmail.com>
2018-12-01 21:17:08 +0200

    add comment in answers(questions)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+  has_many :сomments, as: :object
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20181201000407) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
