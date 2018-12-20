5a0dfc7fc3e94c2add0e1b2349b1510c05c48bac
Civil21 <diusolexander@gmail.com>
2018-12-20 17:50:39 +0200

    add work search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  namespace :pages do
+    post :search
+    get :search
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
