f7c154d238aca034dbfc20caa06c0bd1ec388fb0
z <evgenii.datsiuk@gmail.com>
2019-02-06 21:33:22 +0200

    update validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,10 @@ class Post < ApplicationRecord
   has_many :tags, as: :object, dependent: :destroy
   has_many :votes, as: :object, dependent: :destroy
   has_many :chosens, as: :object, dependent: :destroy
-  validates :name, length: { maximum: 50 }
-  validates :name,:text, presence: true
+  validates :name, length: { maximum: 50, message: "Максимум 50 символів, бо ми лиш вчимо front-end, ok da " }
+ # validates :name,:text, presence: true
+  validates :name, presence: { message: "Something is wrong. Майже, введіть ще раз дані" }
+  validates :text, presence: { message: "Something is wrong. Майже, введіть ще раз дані" }
   include ChosenModul
 
   def all_tags
