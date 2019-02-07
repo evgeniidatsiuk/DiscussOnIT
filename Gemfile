484b205c5d9cac0db84959e71839d817f869d20b
z <evgenii.datsiuk@gmail.com>
2019-02-07 14:47:14 +0200

    for Heroku

--- Gemfile
+++ Gemfile
@@ -83,6 +83,8 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
+group :production do
+gem 'pg' 
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   has_many :tags, as: :object, dependent: :destroy
   has_many :votes, as: :object, dependent: :destroy
   has_many :chosens, as: :object, dependent: :destroy
-  validates :name, length: { maximum: 50, message: "Максимум 50 символів, бо ми лиш вчимо front-end, ok da " }
+  validates :name, length: { maximum: 50, message: "Something is wrong. Максимум 50 символів, бо ми лиш вчимо front-end, ok da " }
  # validates :name,:text, presence: true
   validates :name, presence: { message: "Something is wrong. Майже, введіть ще раз дані" }
   validates :text, presence: { message: "Something is wrong. Майже, введіть ще раз дані" }
