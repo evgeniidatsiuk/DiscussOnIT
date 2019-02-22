ccc3ab04cb6468e8f5d919c16a75dfc5f268547e
evgeniidatsiuk <evgeniidatsiuk@gmail.com>
2019-02-22 22:30:20 +0200

    maybe fixed profile for production

--- app/controllers/userparams_controller.rb
+++ app/controllers/userparams_controller.rb
@@ -1,19 +1,19 @@
 class UserparamsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  #def index; end
 
   def new
     @userparam = Userparam.new
-    @education = Education.new
+  #  @education = Education.new
   end
 
   def create
     @userparam = current_user.build_userparam(profile_params)
-    @education = @userparam.build_education(education_params)
+  #  @education = @userparam.build_education(education_params)
     if @userparam.save
-      redirect_to userparam_path(@userparam.id)
-      @education.save
+      redirect_to userparam_path(current_user.id)
+  #    @education.save
     else
       render "new"
     end
@@ -25,19 +25,19 @@ class UserparamsController < ApplicationController
 
   def edit
     @userparam = current_user.userparam
-    @education = @userparam.education || Education.new
+  #  @education = @userparam.education || Education.new
   end
 
   def update
     @userparam = Userparam.find_by(user_id: current_user.id)
     if @userparam.update(profile_params)
-      if @userparam.education
-        @userparam.education.update(education_params)
-      else
-        @education = @userparam.build_education(education_params)
-        @education.save
-      end
-      redirect_to userparam_path(@userparam.id)
+    #  if @userparam.education
+    #    @userparam.education.update(education_params)
+    #  else
+    #    @education = @userparam.build_education(education_params)
+    #    @education.save
+    #  end
+      redirect_to userparam_path(current_user.id)
     else render 'edit'
     end
 
@@ -49,9 +49,9 @@ class UserparamsController < ApplicationController
     params.require(:userparam).permit(:user_id, :avatar, :nickname, :firstname, :lastname, :age)
   end
 
-  def education_params
-    params.require(:education).permit(:userparam_id, :university_id, :specialty_id, :begin_year, :end_year)
-  end
+#  def education_params
+  #  params.require(:education).permit(:userparam_id, :university_id, :specialty_id, :begin_year, :end_year)
+#  end
 
   def userparam_create; end
 end
